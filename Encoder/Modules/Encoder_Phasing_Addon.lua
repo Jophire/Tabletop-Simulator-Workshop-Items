3102945ad70564992ce76eeb700e85e620b047b5
Jophire <36978900+Jophire@users.noreply.github.com>
2020-11-27 16:28:57 -0500

    Finished updating the last of the modules.

--- Encoder/Modules/Encoder_Phasing_Addon.lua
+++ Encoder/Modules/Encoder_Phasing_Addon.lua
@@ -19,50 +19,71 @@ function registerModule()
 		properties = {
 		propID = pID,
 		name = "Phase Out",
-		dataStruct = {},
+		values = {'mtg_phased'},
 		funcOwner = self,
-		callOnActivate = false,
-		activateFunc =''
+		callOnActivate = true,
+		activateFunc ='phaseOut'
 		}
 		enc.call("APIregisterProperty",properties)
+    value = {
+    valueID = 'mtg_phased',
+    validType = 'boolean',
+    desc = 'MTG: Is the card phased out?.',
+    default = false
+    }
+    enc.call("APIregisterValue",value)
   end
 end
 
 function createButtons(t)
   enc = Global.getVar('Encoder')
   if enc ~= nil then
-    flip = enc.call("APIgetFlip",{obj=t.object})
-    scaler = {x=1,y=1,z=1}--t.object.getScale()
+    flip = enc.call("APIgetFlip",{obj=t.obj})
+    scaler = {x=1,y=1,z=1}--t.obj.getScale()
     temp = " PHASED OUT "
     barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=0,yJust=0})
-    t.object.createButton({
+    t.obj.createButton({
     label=temp, click_function='unPhased', function_owner=self,
     position={(0+offset_x)*flip*scaler.x,0.63*flip*scaler.z,(0+offset_y)*scaler.y}, height=1000, width=1500, font_size=200,color={0,0,0,1},font_color={0.6,0.6,1,1},
-    rotation={0,90,90-90*flip}
+    rotation={0,90,90-90*flip,tooltip="This is Phased Out."}
     })
   end
 end
 
-function unPhased(object,ply)
+function phaseOut(t)
   enc = Global.getVar('Encoder')
   if enc ~= nil then
-    flip = enc.call("APIgetFlip",{obj=object})
+    data = enc.call("APIobjGetPropData",{obj=t.obj,propID=pID})
+		if data.mtg_phased ~= true then
+			data.mtg_phased = true
+    else
+      data.mtg_phased = false
+    end
+    enc.call("APIobjSetPropData",{obj=t.obj,propID=pID,data=data})
+  end
+end
+
+function unPhased(obj,ply)
+  enc = Global.getVar('Encoder')
+  if enc ~= nil and enc.call("APIobjectExists",{obj=obj}) then
+    flip = enc.call("APIgetFlip",{obj=obj})
 		
-		data = enc.call("APIgetObjectData",{obj=object,propID=pID})
-		if data ~= nil and data.enabled == true then
-			data.enabled = false
-			enc.call("APIsetObjectData",{obj=object,propID=pID,data=data})
+		data = enc.call("APIobjGetPropData",{obj=obj,propID=pID})
+		if data.mtg_phased == true then
+			data.mtg_phased = false
+			enc.call("APIobjSetPropData",{obj=obj,propID=pID,data=data})
+      enc.call("APIobjDisableProp",{obj=obj,propID=pID})
 		end
 		if type(ply) == "string" then
 			local selection =Player[ply].getSelectedObjects()
 			if selection ~= nil then
 				for k,v in pairs(selection) do
-					if v ~= tar and enc.call("APIobjectExist",{obj=v}) == true then 
+					if v ~= tar and enc.call("APIobjectExists",{obj=v}) == true then 
 						unPhased(v,0)
 					end
 				end
 			end
 		end
-    enc.call("APIrebuildButtons",{obj=object})
+    enc.call("APIrebuildButtons",{obj=obj})
   end
 end
