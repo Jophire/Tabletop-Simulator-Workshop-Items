8d26ad21c73e5d8c9c8d9a2cd23194b88c1796ce
Jophire <36978900+Jophire@users.noreply.github.com>
2020-11-27 17:02:31 -0500

    Some more commenting and cleanup

--- Encoder/Modules/Encoder_DropOn_Counter.lua
+++ Encoder/Modules/Encoder_DropOn_Counter.lua
@@ -38,19 +38,19 @@ function onCollisionEnter(c)
           else
             print(m..' is not a recognized operator.')
           end
-        elseif enc.call('APIpropertyExists',{propID=k} then
+        elseif enc.call('APIpropertyExists',{propID=k}) then
           if enc.call('APIobjIsPropEnabled',{obj=obj,propID=k}) ~= v then
             enc.call('APItoggleProperty',{obj=obj,propID=k})
           end
         else
           local txt = ''
-          for k,v in pairs(enc.call('APIlistProps',{})) do
+          for k,v in pairs(enc.call('APIlistProps')) do
             txt = txt..v.."\n"
           end
-          for k,v in pairs(enc.call('APIlistValues',{})) do
+          for k,v in pairs(enc.call('APIlistValues')) do
             txt = txt..v.."\n"
           end
-          Notes.editNotebookTab({index=0,title="Encoder: List of values and properties",body=txt,color='Grey'})
+          Notes.addNotebookTab({title="Encoder: List of values and properties",body=txt,color='Grey'})
         end
       end
       enc.call('APIobjSetAllData',{obj=obj,data=cd})
