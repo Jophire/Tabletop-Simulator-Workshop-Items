919854272ec442216f6e2cf0ac71362eb115a6b4
Jophire <36978900+Jophire@users.noreply.github.com>
2021-01-25 22:20:52 -0500

    Consolidating Stuff
    Consolidated Power Toughness stats into 1 module.
    Fixed the one button the encoder creates to follow style.
    Depreciated oneOneCounters.

--- Encoder/Modules/Encoder_PowerToughness_Addon.lua
+++ Encoder/Modules/Encoder_PowerToughness_Addon.lua
@@ -3,7 +3,7 @@ by Tipsy Hobbit//STEAM_0:1:13465982
 This module adds only 1/1 Counters
 ]]
 pID = "MTG_Power_Toughness"
-version = '1.2.3'
+version = '1.2.4'
 UPDATE_URL='https://raw.githubusercontent.com/Jophire/Tabletop-Simulator-Workshop-Items/master/Encoder/Modules/Encoder_PowerToughness_Addon.lua'
 Style={}
 
@@ -19,7 +19,7 @@ function registerModule()
     properties = {
     propID = pID,
     name = "Power/Toughness",
-    values = {'power_base','toughness_base','moduleMod','moduleMath'},
+    values = {'power_base','toughness_base','power_mod','toughness_mod','oneOneCounter','moduleMod','moduleMath'},
     funcOwner = self,
     tags="base_stat,basic",
     activateFunc ='callEditor'
@@ -51,6 +51,13 @@ function registerModule()
     valueID = 'toughness_mod', 
     validType = 'number',
     desc = 'MTG:Static modifiers to base toughness.',
+    default = 0       
+    }
+    enc.call("APIregisterValue",value)
+    value = {
+    valueID = 'oneOneCounter', 
+    validType = 'number',
+    desc = 'Amount of +1/+1 Counters.',   
     default = 0       
     }
     enc.call("APIregisterValue",value)
@@ -92,52 +99,94 @@ function createButtons(t)
     scaler = {x=1,y=1,z=1}--t.obj.getScale()
     editing = enc.call("APIgetEditing",{obj=t.obj})
     if editing == nil then
-      temp = ""..data.power_base..'/'..data.toughness_base..""
+      temp = ""..data.power_base+data.power_mod+data.oneOneCounter..'/'..data.toughness_base+data.toughness_mod+data.oneOneCounter..""
       barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=90,max_len=90,xJust=1,yJust=0})
       t.obj.createButton(Style.new{
       label=temp, click_function='toggleEditor', function_owner=self,
       position={(1.1+offset_x)*flip*scaler.x,0.28*flip*scaler.z,(1.4+offset_y)*scaler.y}, height=170, width=barSize, font_size=fsize,
-      rotation={0,0,90-90*flip}
+      rotation={0,0,90-90*flip},tooltip="Base: "..data.power_base.."/"..data.toughness_base.."\nStatic: "..data.power_mod.."/"..data.toughness_mod.."\n Counters: "..(data.oneOneCounter > 0 and "+"..data.oneOneCounter or data.oneOneCounter)
       })
     elseif editing == pID then
       t.obj.createButton(Style.new{
+      label="Base\nStats", click_function='doNothing', function_owner=self,tooltip="Base Stats",
+      position={-1.05,0.28*flip*scaler.z,-1}, height=150, width=300, font_size=90,rotation={0,-90,90-90*flip}
+      })
+      t.obj.createButton(Style.new{
       label="/", click_function='doNothing', function_owner=self,
-      position={0,0.28*flip*scaler.z,-0.8}, height=0, width=0, font_size=400,rotation={0,0,90-90*flip}
+      position={0,0.28*flip*scaler.z,-1}, height=0, width=0, font_size=400,rotation={0,0,90-90*flip}
       })
       temp = ""..data.power_base
       barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=1,yJust=0})
       t.obj.createButton(Style.new{
       label=temp, click_function='cyclePower', function_owner=self,
-      position={(-0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.8+offset_y}, height=400, width=barSize > 400 and barSize or 400, 
-      font_size=fsize,rotation={0,0,90-90*flip}, tooltip='Modify Power'
+      position={(-0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-1+offset_y}, height=300, width=barSize > 500 and barSize or 500, 
+      font_size=fsize,rotation={0,0,90-90*flip}, tooltip='Modify Base Power'
       })  
       temp = ""..data.toughness_base
       barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=-1,yJust=0})
       t.obj.createButton(Style.new{
       label= temp, click_function='cycleToughness', function_owner=self,
-      position={(0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.8+offset_y}, height=400, width=barSize > 400 and barSize or 400, 
+      position={(0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-1+offset_y}, height=300, width=barSize > 500 and barSize or 500, 
+      font_size=fsize,rotation={0,0,90-90*flip}, tooltip='Modify Base Toughness'
+      })
+      
+      t.obj.createButton(Style.new{
+      label="Static\nMod", click_function='doNothing', function_owner=self,tooltip="Static Modifiers",
+      position={-1.05,0.28*flip*scaler.z,-0.4}, height=150, width=300, font_size=90,rotation={0,-90,90-90*flip}
+      })
+      t.obj.createButton(Style.new{
+      label="/", click_function='doNothing', function_owner=self,
+      position={0,0.28*flip*scaler.z,-0.4}, height=0, width=0, font_size=400,rotation={0,0,90-90*flip}
+      })
+      temp = ""..data.power_mod
+      barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=1,yJust=0})
+      t.obj.createButton(Style.new{
+      label=temp, click_function='cyclePowerMod', function_owner=self,
+      position={(-0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.4+offset_y}, height=300, width=barSize > 500 and barSize or 500, 
+      font_size=fsize,rotation={0,0,90-90*flip}, tooltip='Modify Power'
+      })  
+      temp = ""..data.toughness_mod
+      barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=-1,yJust=0})
+      t.obj.createButton(Style.new{
+      label= temp, click_function='cycleToughnessMod', function_owner=self,
+      position={(0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.4+offset_y}, height=300, width=barSize > 500 and barSize or 500, 
       font_size=fsize,rotation={0,0,90-90*flip}, tooltip='Modify Toughness'
       })
       
       t.obj.createButton(Style.new{
+      label="+1/+1\nCounter", click_function='doNothing', function_owner=self,tooltip="+1/+1 and -1/-1 Counters",
+      position={-1.05,0.28*flip*scaler.z,0.20}, height=150, width=300, font_size=90,rotation={0,-90,90-90*flip}
+      })
+      temp = ((data.oneOneCounter >= 0) and "+" or "")..data.oneOneCounter
+      temp = ""..temp
+      barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=1,yJust=0})
+      t.obj.createButton(Style.new{
+      label=temp, click_function='cycleOneOneCounter', function_owner=self,
+      position={0,0.28*flip*scaler.z,0.20}, height=300, width=barSize > 900 and barSize or 900, font_size=fsize,rotation={0,0,90-90*flip},
+      tooltip = 'Modify 1/1 counters.'
+      })
+      
+      t.obj.createButton(Style.new{
       label= data.moduleMath, click_function='cycleMath', function_owner=self,
-      position={-0.4*flip,0.28*flip*scaler.z,0}, height=400, width=400, font_size=240,rotation={0,0,90-90*flip},
+      position={-0.35*flip,0.28*flip*scaler.z,0.9}, height=300, width=300, font_size=240,rotation={0,0,90-90*flip},
       tooltip = data.moduleMath == '+-' and 'Add or Subtract '..data.moduleMod or 'Multiply or Divide by '..data.moduleMod
       })
       t.obj.createButton(Style.new{
       label= data.moduleMod, click_function='cycleMod', function_owner=self,
-      position={0.4*flip,0.28*flip*scaler.z,0}, height=400, width=400, font_size=240,rotation={0,0,90-90*flip},
+      position={0.35*flip,0.28*flip*scaler.z,0.9}, height=300, width=300, font_size=240,rotation={0,0,90-90*flip},
       tooltip = data.moduleMath == '+-' and 'Add or Subtract '..data.moduleMod or 'Multiply or Divide by '..data.moduleMod
       })
       t.obj.createButton(Style.new{
       label= "Reset", click_function='resetValues', function_owner=self,
-      position={0*flip,0.28*flip*scaler.z,1.0}, height=200, width=600, font_size=240,rotation={0,0,90-90*flip},
+      position={0*flip,0.28*flip*scaler.z,1.4}, height=200, width=600, font_size=240,rotation={0,0,90-90*flip},
       tooltip = 'Left click to reset values, right click to reset editor.'
       })
     end
   end
 end
 
+function doNothing()
+end
 function toggleEditor(obj,ply)
   enc = Global.getVar('Encoder')
   if enc ~= nil then
@@ -145,7 +194,6 @@ function toggleEditor(obj,ply)
     enc.call("APIrebuildButtons",{obj=obj})
   end
 end
-
 function callEditor(obj,ply)
   enc.call("APItoggleProperty",{obj=obj,propID=pID})
   if enc.call("APIobjIsPropEnabled",{obj=obj,propID=pID}) then
@@ -154,7 +202,6 @@ function callEditor(obj,ply)
     enc.call("APIrebuildButtons",{obj=obj})
   end
 end
-
 function toggleEditClose(obj,ply)
   enc = Global.getVar('Encoder')
   if enc ~= nil then
@@ -173,24 +220,41 @@ function updateEditDisp(obj)
     temp = ""..data.power_base
     barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=1,yJust=0})
     obj.editButton({
-    position={(-0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.8+offset_y},
-    index=1,label=temp,width=barSize > 400 and barSize or 400, font_size=fsize})
+    position={(-0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-1+offset_y},
+    index=2,label=temp,width=barSize > 500 and barSize or 500, font_size=fsize})
     
     temp = ""..data.toughness_base
     barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=-1,yJust=0})
     obj.editButton({
-    position={(0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.8+offset_y},
-    index=2,label=temp,width=barSize > 400 and barSize or 400, font_size=fsize})
+    position={(0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-1+offset_y},
+    index=3,label=temp,width=barSize > 500 and barSize or 500, font_size=fsize})
+    
+    temp = ""..data.power_mod
+    barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=1,yJust=0})
+    obj.editButton({
+    position={(-0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.4+offset_y},
+    index=6,label=temp,width=barSize > 500 and barSize or 500, font_size=fsize})
+    temp = ""..data.toughness_mod
+    barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=-1,yJust=0})
+    obj.editButton({
+    position={(0.2+offset_x*2.6)*flip,0.28*flip*scaler.z,-0.4+offset_y},
+    index=7,label=temp,width=barSize > 500 and barSize or 500, font_size=fsize})
+    
+    temp = ((data.oneOneCounter >= 0) and "+" or "")..data.oneOneCounter
+    temp = ""..temp
+    barSize,fsize,offset_x,offset_y = enc.call('APIformatButton',{str=temp,font_size=400,max_len=90,xJust=0,yJust=0})
+    obj.editButton({
+    index=9,label=temp,width=barSize > 900 and barSize or 900, font_size=fsize})
     
     obj.editButton({
-    index=3,label=data.moduleMath,tooltip = data.moduleMath == '+-' and 'Add or Subtract '..data.moduleMod or 'Multiply or Divide by '..data.moduleMod})
+    index=10,label=data.moduleMath,tooltip = data.moduleMath == '+-' and 'Add or Subtract '..data.moduleMod or 'Multiply or Divide by '..data.moduleMod})
     
     obj.editButton({
-    index=4,label=data.moduleMod,tooltip = data.moduleMath == '+-' and 'Add or Subtract '..data.moduleMod or 'Multiply or Divide by '..data.moduleMod})
+    index=11,label=data.moduleMod,tooltip = data.moduleMath == '+-' and 'Add or Subtract '..data.moduleMod or 'Multiply or Divide by '..data.moduleMod})
   end
 end
 
-function cyclePower(obj,ply,alt)
+function cycleValue(obj,ply,alt,val)
   enc = Global.getVar('Encoder')
   if enc ~= nil then
     data = enc.call("APIobjGetPropData",{obj=obj,propID=pID})
@@ -198,18 +262,18 @@ function cyclePower(obj,ply,alt)
     local mMat = type(ply)=="string" and data.moduleMath or ply['mat']
     if mMat == '+-' then
       if alt == false then
-        data.power_base = data.power_base+mMod
+        data[val] = data[val]+mMod
       else
-        data.power_base = data.power_base-mMod
+        data[val] = data[val]-mMod
       end
     else
       if alt == false then
-        data.power_base = data.power_base*mMod
+        data[val] = data[val]*mMod
       else
         if data.moduleMod == 0 then
           data.moduleMod = 1
         end
-        data.power_base = data.power_base/mMod
+        data[val] = data[val]/mMod
       end
     end
     enc.call("APIobjSetPropData",{obj=obj,propID=pID,data=data})
@@ -219,7 +283,7 @@ function cyclePower(obj,ply,alt)
       if selection ~= nil then
         for k,v in pairs(selection) do
           if v ~= obj and enc.call("APIobjectExists",{obj=v}) == true then 
-            cyclePower(v,{mod=mMod,mat=mMat},alt)
+            cycleValue(v,{mod=mMod,mat=mMat},alt,val)
           end
         end
       end
@@ -227,45 +291,24 @@ function cyclePower(obj,ply,alt)
       enc.call("APIrebuildButtons",{obj=obj})
     end
   end
+end
+function cyclePower(obj,ply,alt)
+  cycleValue(obj,ply,alt,'power_base')
 end
 function cycleToughness(obj,ply,alt)
-  enc = Global.getVar('Encoder')
-  if enc ~= nil then
-    data = enc.call("APIobjGetPropData",{obj=obj,propID=pID})
-    local mMod = type(ply)=="string" and data.moduleMod or ply['mod']
-    local mMat = type(ply)=="string" and data.moduleMath or ply['mat']
-    if mMat == '+-' then
-      if alt == false then
-        data.toughness_base = data.toughness_base+mMod
-      else
-        data.toughness_base = data.toughness_base-mMod
-      end
-    else
-      if alt == false then
-        data.toughness_base = data.toughness_base*mMod
-      else
-        if data.moduleMod == 0 then
-          data.moduleMod = 1
-        end
-        data.toughness_base = data.toughness_base/mMod
-      end
-    end
-    enc.call("APIobjSetPropData",{obj=obj,propID=pID,data=data})
-    if type(ply) == "string" then
-      updateEditDisp(obj)
-      local selection =Player[ply].getSelectedObjects()
-      if selection ~= nil then
-        for k,v in pairs(selection) do
-          if v ~= obj and enc.call("APIobjectExists",{obj=v}) == true then 
-            cycleToughness(v,{mod=mMod,mat=mMat},alt)
-          end
-        end
-      end
-    else
-      enc.call("APIrebuildButtons",{obj=obj})
-    end
-  end
+  cycleValue(obj,ply,alt,'toughness_base')
 end
+function cyclePowerMod(obj,ply,alt)
+  cycleValue(obj,ply,alt,'power_mod')
+end
+function cycleToughnessMod(obj,ply,alt)
+  cycleValue(obj,ply,alt,'toughness_mod')
+end
+function cycleOneOneCounter(obj,ply,alt)
+  cycleValue(obj,ply,alt,'oneOneCounter')
+end
+
+
 function cycleMath(obj,ply,alt)
   enc = Global.getVar('Encoder')
   if enc ~= nil then
@@ -299,6 +342,9 @@ function resetValues(obj,ply,alt)
     if alt == false then
       enc.call("APIobjDefaultValue",{obj=obj,valueID='toughness_base'})
       enc.call("APIobjDefaultValue",{obj=obj,valueID='power_base'})
+      enc.call("APIobjDefaultValue",{obj=obj,valueID='toughness_mod'})
+      enc.call("APIobjDefaultValue",{obj=obj,valueID='power_mod'})
+      enc.call("APIobjDefaultValue",{obj=obj,valueID='oneOneCounter'})
     else
       enc.call("APIobjDefaultValue",{obj=obj,valueID='moduleMath'})
       enc.call("APIobjDefaultValue",{obj=obj,valueID='moduleMod'})
